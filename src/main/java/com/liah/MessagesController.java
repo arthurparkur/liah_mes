10710df443ee6a3db9b1e259123ac48ffd3378ec
Arthur Parkur <flava1@rambler.ru>
2018-03-17 19:14:37 +0300

    init

---
+++ src/main/java/com/liah/MessagesController.java
@@ -0,0 +1,16 @@
+package com.liah;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MessagesController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(Message message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        System.out.println(message.getName());
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+}
